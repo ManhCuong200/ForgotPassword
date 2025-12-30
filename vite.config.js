2cfd54701425fc3a4268fea5524e270126047cfd
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-12-30 20:16:48 +0700

    feat: add PWA support with vite-plugin-pwa and update assets
    - Added vite-plugin-pwa to enable PWA features.
    - Updated Vite configuration to include PWA manifest and icons.
    - Removed old icon and added new PWA icon assets.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Tên Ứng Dụng Quên Mật Khẩu",
+        short_name: "AppShortName",
+        description: "Mô tả ngắn về ứng dụng React PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     host: true,
     port: 5173,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
-  }
-
-})
+  },
+});
